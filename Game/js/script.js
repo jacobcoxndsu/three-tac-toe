97ce72f5bda93ea690277d8d74092b6d56843374
Thor Hanson <hansonthor@gmail.com>
2019-05-08 22:31:57 -0500

    Cleaning up

--- Game/js/script.js
+++ Game/js/script.js
@@ -1,3 +1,8 @@
+/*
+Copyright 2018 Jacob Cox
+Released under MIT License, included with this project.
+*/
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
